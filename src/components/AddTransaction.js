de3541c621be07f0ac596578773a07401022b8a8
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-03-27 20:57:38 +0100

    expense tracker first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import React from 'react'
+
+const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+  return (
+    <div>
+      <h3>Add new Transaction</h3>
+      <form >
+        <div className='form-control'>
+            <label htmlFor = 'text'>Text</label>
+            <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...' />
+
+        </div>
+
+        <div className='form-control'>
+            <label htmlFor='amount'
+            >Amount <br />
+            (negative - expense, positive - income)
+            </label>
+
+            <input type='number'  placeholder='Enter amount' />
+
+        </div>
+          <button className='btn'>Add Transaction</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTransaction
